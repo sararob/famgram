38b225f2ca9272b4104834605ad24ccde97743a1
sararob <sararob@brandeis.edu>
2013-04-23 11:48:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Famgram::Application.config.secret_token = '90a5e09b1b1d66bfd56e52cfbc7da6ebd4fec1595c7121ce56d78bb303dda6359238e0bfa6124c14e6ab1d9f0a5132fc2a8ed6bacae316bcc34d8f72955b4ed9'
