8c6e916d58d83d32396ce31a64c8b1876ad533d1
sararob <sararob@brandeis.edu>
2013-04-24 10:57:38 -0700

    Write rspec tests for home page

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -6,6 +6,12 @@ describe "Pages" do
   
   describe "Home page" do
     before { visit root_path }
+    
     it { should have_selector('h2', text: 'FamGram') }
+    it { should have_selector('h2', text: 'Share Your Life') }
+    it { should have_selector('h2', text: 'For the Whole Family') }
+    it { should have_selector('h2', text: 'In the Mailbox') }
+    
+    it { should have_link("FamGram", href: root_path) }
   end
 end
